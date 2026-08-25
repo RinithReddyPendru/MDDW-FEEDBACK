90fcc05db6eb8bcfb174a36321907a98f5332ece
Pendru Rinith Reddy <rinithreddypendru@gmail.com>
2026-08-25 16:32:03 +0530

    feat: Add elegant Lotus mascot

--- src/components/Mascot.tsx
+++ src/components/Mascot.tsx
@@ -1,4 +1,4 @@
-﻿import React from "react";
+import React from "react";
 
 export default function Mascot({ emotion }: { emotion: string }) {
   let message = "I'm here to help!";
@@ -26,16 +26,16 @@ export default function Mascot({ emotion }: { emotion: string }) {
             {message}
           </div>
           
-          <div className={\w-20 h-20 rounded-full border-4 border-white flex items-center justify-center cursor-pointer transition-all duration-500 ease-out relative \\}>
+          <div className={`w-20 h-20 rounded-full border-4 border-white flex items-center justify-center cursor-pointer transition-all duration-500 ease-out relative ${containerClasses}`}>
             {/* Beautiful Lotus SVG */}
             <svg viewBox="0 0 100 100" className="w-12 h-12 relative z-10 drop-shadow-md transition-all duration-500">
               {/* Outer Petals */}
-              <path d="M 50 90 C 20 90 10 60 10 50 C 30 50 40 70 50 90 Z" className={\\ transition-colors duration-500\} />
-              <path d="M 50 90 C 80 90 90 60 90 50 C 70 50 60 70 50 90 Z" className={\\ transition-colors duration-500\} />
+              <path d="M 50 90 C 20 90 10 60 10 50 C 30 50 40 70 50 90 Z" className={`${petalColor} transition-colors duration-500`} />
+              <path d="M 50 90 C 80 90 90 60 90 50 C 70 50 60 70 50 90 Z" className={`${petalColor} transition-colors duration-500`} />
               
               {/* Middle Petals */}
-              <path d="M 50 90 C 25 80 20 40 30 30 C 40 45 45 70 50 90 Z" className={\\ transition-colors duration-500\} />
-              <path d="M 50 90 C 75 80 80 40 70 30 C 60 45 55 70 50 90 Z" className={\\ transition-colors duration-500\} />
+              <path d="M 50 90 C 25 80 20 40 30 30 C 40 45 45 70 50 90 Z" className={`${centerColor} transition-colors duration-500`} />
+              <path d="M 50 90 C 75 80 80 40 70 30 C 60 45 55 70 50 90 Z" className={`${centerColor} transition-colors duration-500`} />
               
               {/* Center Petal */}
               <path d="M 50 90 C 40 70 40 30 50 15 C 60 30 60 70 50 90 Z" className="fill-white opacity-90" />
